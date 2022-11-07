12b5818a735932b35e54f6d4b881181b268c8844
Ankit Jain <ankii.jain@gmail.com>
2022-11-07 08:08:32 +0530

    fix issue

--- variables.tf
+++ variables.tf
@@ -18,10 +18,7 @@ EOF
     default     = "SYMMETRIC_DEFAULT"
 
     validation {
-      condition = contains(["SYMMETRIC_DEFAULT", 
-                            "HMAC_224", "HMAC_256", "HMAC_384", "HMAC_512",
-                            "RSA_2048", "RSA_3072", "RSA_4096",
-                            "ECC_NIST_P256", "ECC_NIST_P384", "ECC_NIST_P521", "ECC_SECG_P256K1"], 
+      condition = contains(["SYMMETRIC_DEFAULT", "HMAC_224", "HMAC_256", "HMAC_384", "HMAC_512", "RSA_2048", "RSA_3072", "RSA_4096", "ECC_NIST_P256", "ECC_NIST_P384", "ECC_NIST_P521", "ECC_SECG_P256K1"], 
                             var.key_usage)
       error_message = <<EOF
 Valid values are as follows:
